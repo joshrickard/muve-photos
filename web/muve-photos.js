18143a33f248bd227b280ca4d065bd28d243bc48
Josh Rickard <josh.rickard@gmail.com>
2015-02-22 16:07:47 -0600

    add photo rotations
    One photo per page, rotate photos at 5 second interval, sort photos by
    date created.

--- web/muve-photos.js
+++ web/muve-photos.js
@@ -3,9 +3,10 @@
 
   var muvePhotos = {
     currentPage: 0,
-    photosPerPage: 3,
+    photosPerPage: 1,
     options: { },
     photos: [ ],
+    rotationDelay: 5000,
 
     init: function(config) {
       muvePhotos.options = config || { };
@@ -14,6 +15,18 @@
 		    Tabletop.init({
 			    key: config.key,
 			    callback: function(data, tabletop) {
+            for (var i = 0; i<data.length; i++) {
+              data[i].created_parsed = Date.parse( data[i].created );
+            }
+
+            data.sort( function(a, b) {
+              if (a.created_parsed < b.created_parsed)
+                return -1;
+              if (a.created_parsed > b.created_parsed)
+                return 1;
+              return 0;
+            });
+
             muvePhotos.showInfo(data, config.hashtag);
           },
 			    simpleSheet: true
@@ -36,7 +49,7 @@
     },
 
     renderPage: function() {
-      for (var i = 0; i < 3; i++) {
+      for (var i = 0; i < muvePhotos.photosPerPage; i++) {
         var index = muvePhotos.currentPage * muvePhotos.photosPerPage + i;
         var link = document.getElementById('muve_photo_a_' + i);
         var img = document.getElementById('muve_photo_i_' + i);
@@ -53,23 +66,35 @@
       }
     },
 
+    rotate: function() {
+      (function(){
+        muvePhotos.currentPage += 1;
+        if (muvePhotos.currentPage > muvePhotos.photos.length - 1)
+          muvePhotos.currentPage = 0;
+
+        muvePhotos.renderPage();
+
+        setTimeout(arguments.callee, muvePhotos.rotationDelay);
+      })();
+    },
+
     showInfo: function(data, hashtag) {
       var container = document.getElementById( muvePhotos.options.container );
       var i = 0;
 
-      for (i = 0; i < 3; i++) {
+      for (i = 0; i < muvePhotos.photosPerPage; i++) {
         container.innerHTML = container.innerHTML +
           "<a href='' id='muve_photo_a_" + i + "' target='_blank'>" +
             "<img class='" + muvePhotos.options.classes + "' id='muve_photo_i_" + i + "' style='" + muvePhotos.options.styles + "' src=''>" +
           "</a>";
       }
 
-      container.innerHTML = container.innerHTML +
-        "<div class='controls'>" +
-          "<a href='#' onclick='MuvePhotos.previousPage(); return false' style='color: black;'>Previous</a>" +
-          " &#8212; " +
-          "<a href='#' onclick='MuvePhotos.nextPage(); return false' style='color: black;'>Next</a>" +
-        "</div>";
+      // container.innerHTML = container.innerHTML +
+      //   "<div class='controls'>" +
+      //     "<a href='#' onclick='MuvePhotos.previousPage(); return false' style='color: black;'>Previous</a>" +
+      //     " &#8212; " +
+      //     "<a href='#' onclick='MuvePhotos.nextPage(); return false' style='color: black;'>Next</a>" +
+      //   "</div>";
 
 		  for (i = 0; i < data.length; i++) {
 
@@ -85,6 +110,8 @@
 		  }
 
       muvePhotos.renderPage();
+
+      setTimeout(muvePhotos.rotate, muvePhotos.rotationDelay);
     }
 
   };
